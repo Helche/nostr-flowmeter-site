19b1f1990a4b8bd57a2c60231fad93699e6e4654
Helche <xihe0163@yahoo.co.jp>
2025-12-06 11:45:32 +0900

    temp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,22 +9,19 @@
 </div>
 <div class="row max-width mx-auto pt-3">
   <div class="col-12">
-    このサイトは Nostr 日本リレーの流速をグラフ表示化しています。<br />
-    流速ちゃんがリレー上に投稿したKind 30078 をデータベースとしたシステムで、nostr-key-value
-    プロジェクトのサンプルとしても提供されています。<br />
+    このサイトでは、一部の Nostr リレーの流速を計測し、集計、グラフ化しています。<br />
+    このサイトは<a href="https://github.com/ShinoharaTa/nostr-flowmeter-site" target="_blank">ShinoharaTa/nostr-flowmeter-site</a>を基としています。<br />
 
     <br />
     サイト管理者:
     <a
-      href="https://nostx.shino3.net/npub1l60d6h2uvdwa9yq0r7r2suhgrnsadcst6nsx2j03xwhxhu2cjyascejxe5"
+      href="https://nostx.io/npub1helchedzyu8acr3e987498krud4c9jze22d7z4hdrw5fhpdgxxkskqnyj6"
       target="_blank"
-      class="text-success">shino3（しのさん）</a
-    ><br />
-    ページソース・質問・お問い合わせ:
+      class="text-success">Χέλτσε Αγνοσος(Helche)</a><br />
+    ページソース:
     <a
       href="https://github.com/ShinoharaTa/nostr-flowmeter-site"
-      target="_blank">GitHub</a
-    >
+      target="_blank">GitHub</a>
     <!-- <br>
     <br>
     ※ このサイトに掲載されているロゴ・イラストは許諾を得て掲載させていただいております。<br> -->
@@ -60,7 +57,7 @@
       <tr class="bg-sub">
         <th>観測システム</th>
       </tr>
-      <tr>
+      <!-- <tr>
         <td
           ><img
             src="https://i.gyazo.com/5d38eb6b55fbacfa81f836eb191b1a76.jpg"
@@ -68,33 +65,33 @@
             class="img-fluid"
           /></td
         >
-      </tr>
+      </tr> -->
       <tr>
         <td
           ><a
             href="https://nostx.shino3.net/npub150qnaaxfan8auqdajvn292cgk2khm3tl8dmakamj878z44a6yntqk7uktv"
             target="_blank"
-            class="text-success">流速ちゃん</a
+            class="text-success">流速たん</a
           ></td
         >
       </tr>
       <tr>
         <td
-          >日本リレーの流速を毎分計測します。<br />計測したデータはNIP-78 (Kind:
+          >指定されているリレーの流速を毎分計測します。<br />計測したデータはNIP-78 (Kind:
           30078) に投稿され、記録されていきます。<br /></td
         >
       </tr>
-      <tr>
+      <!-- <tr>
         <td
           >Illust by <a
             href="https://nostx.shino3.net/npub1tuqsl6l8xzly95vv80um7wsnt7gxy8w9wgt4khp4wyv4xwhfw44slm93e9"
             target="_blank">© りら</a
           ></td
         >
-      </tr>
+      </tr> -->
     </table>
   </div>
-  <div class="col-12 mt-4">
+  <!-- <div class="col-12 mt-4">
     <div class="h5">野洲田川水系 河川構成図</div>
     <div><img src="/image/river_image.png" alt="" class="img-fluid" /></div>
     <div class="text-center">
@@ -104,7 +101,7 @@
       >
     </div>
   </div>
-  <div class="p-4" />
+  <div class="p-4" /> -->
 </div>
 
 <!-- {/if} -->
